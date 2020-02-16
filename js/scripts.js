4dd24e98d7138febda9143452d8dbe0aba55cb9f
Kent McKinney and Winter Runion <kent@>
2020-02-16 14:04:58 -0800

    Add function to determine win condition.

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,23 @@ const board = {
   nine: ""
 }
 
+function winCondition(board) {
+  let winner = "";
+  if ((board.one === board.two) && (board.two === board.three)) { winner = board.one; }
+  if ((board.four === board.five) && (board.five === board.six)) { winner = board.four; }
+  if ((board.seven === board.eight) && (board.eight === board.nine)) { winner = board.seven; }
+  if ((board.one === board.four) && (board.four === board.seven)) { winner = board.one; }
+  if ((board.two === board.five) && (board.five === board.eight)) { winner = board.two; }
+  if ((board.three === board.six) && (board.six === board.nine)) { winner = board.three; }
+  if ((board.one === board.five) && (board.five === board.nine)) { winner = board.one; }
+  if ((board.three === board.five) && (board.five === board.seven)) { winner = board.three; }
+  
+  if (winner) {
+    console.log(`${winner} wins!`);
+    return winner;
+  }
+}
+
 // Player 1 = X; Player 2 = O;
 let player = 0;
 
@@ -33,17 +50,15 @@ $(document).ready(function() {
       board[clickedItem] = "O";
       player = 1;
     }
-
     
-    //console.log(board);
+    console.log(board);
      
     
 
-    
+    winCondition(board); 
     // Toggle players
     $("#player1").toggleClass('active-player');
     $("#player2").toggleClass('active-player');
-
     });
 
  
