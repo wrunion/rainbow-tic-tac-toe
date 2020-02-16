c3128f4e508292513fa2067490fe56cf888c819a
Kent McKinney and Winter Runion <kent+winter@>
2020-02-16 12:38:09 -0800

    Add player divs and toggleClass methods.

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,21 @@ const board = {
 
 // UI logic ---------------------
 $(document).ready(function() {
+  $("#player1").toggleClass('active-player');
+
   $(".square").click(function(e) {
+        //console.log(`You have clicked square number ${this.id}`);
     $(this).css('background-color', 'green');
     let clickedItem = this.id;    
     $(this).text("X");
     board[clickedItem] = "X";
+    console.log(board);
+
+    // Toggle players
+    $("#player1").toggleClass('active-player');
+    $("#player2").toggleClass('active-player');
+
     });
+
+ 
 }); 
