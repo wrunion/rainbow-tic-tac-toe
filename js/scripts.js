9c940228d1f65da3fd34734c765c9879c73fa50b
Winter Runion <winterrunion@gmail.com>
2020-02-17 19:15:36 -0800

    Add branching logic to click function to close loopholes.

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,7 @@ function winCondition(board) {
   if ((board.three) && (board.three === board.six) && (board.six === board.nine)) { return board.three; }
   if ((board.one) && (board.one === board.five) && (board.five === board.nine)) { return board.one; }
   if ((board.three) && (board.three === board.five) && (board.five === board.seven)) { return board.three; } 
+  return "";
 }
 
 // Player 1 = X; Player 2 = O;
@@ -34,29 +35,27 @@ $(document).ready(function() {
 
   $(".square").click(function(e) {
     let winner = winCondition(board);
-    console.log(winner);
+    if ((winner === "X") || (winner === "O")) return; 
 
-    if (winner) return 
-
-
-    let clickedItem = this.id;    
+    // let clickedItem = this.id;    
     
+    let textVal = $(this).text();
+
+    if ((textVal === "X") || (textVal === "O")) return;
+
     if (player) {
-      $(this).text("X");
-      if (this.id === "one") {
-
-      }
-      board[clickedItem] = "X";
-      player = 0;
+        $(this).text("X");
+        board[this.id] = "X";
+        player = 0;
     } else {
-      $(this).text("O");
-      board[clickedItem] = "O";
-      player = 1;
+        $(this).text("O");
+        board[this.id] = "O";
+        player = 1;
     }
-    
+  
     // Check for winner; return win message if true;
-    winCondition(board);
-    if (winner) {
+    winner = winCondition(board);
+    if ((winner === "X") || (winner === "O")) {
       $("#result-div").append(`${winner} wins!`);
     }
    
